ca43dbe7bbd397ce91964f05b5406f029045c4cd
jonssa <maciej.jonca@vp.pl>
2020-08-18 09:27:58 +0200

    Minesweeper commit

--- minesweeper.py
+++ minesweeper.py
@@ -3,57 +3,96 @@ import random
 
 class Board:
     def __init__(self):
-        self.board_size = 5
+        self.board_x_size = 10
+        self.board_y_size = 10
+        self.board_size = self.board_x_size * self.board_y_size
         self.mines_count = 3
         self.grid = []
-        self.depth = 5
+        self.depth = 3
 
     def set_size_and_mines(self):
         size = input("New start board size: ")
         mines = input("New start mines count: ")
         print(" ")
         self.mines_count = int(mines)
-        self.board_size = int(size)
+        self.board_size = self.board_x_size = self.board_y_size = int(size)
 
     def create_grid(self):
-        #self.grid = [[[Cell] for row in range(int(self.board_size))] for column in range(int(self.board_size))]
         for row in range(int(self.board_size)):
             for column in range(int(self.board_size)):
                 self.grid.append(Cell(column, row))
 
     def show_grid(self):
-        for row in range(int(self.board_size) + 2 * self.depth):
-            for column in range(int(self.board_size) + 2 * self.depth):
-                if next((obj for obj in self.grid if obj.x_pos + self.depth == row and obj.y_pos + self.depth == column), False):
+        for row in range(int(self.board_y_size) + 2 * self.depth):
+            for column in range(int(self.board_x_size) + 2 * self.depth):
+                if next((obj for obj in self.grid if obj.x_pos + self.depth == row and obj.y_pos + self.depth == column)
+                        , False):
                     item = " #"
                 else:
-                    item = " U"
+                    item = " ."
                 print(item, sep=' ', end='', flush=True)
             print("")
 
     def refresh_grid(self):
-        for row in range(self.board_size + 2 * self.depth):
-            for column in range(self.board_size + 2 * self.depth):
+        for row in range(self.board_y_size + 2 * self.depth):
+            for column in range(self.board_x_size + 2 * self.depth):
                 space = " "
-                if next((obj for obj in self.grid if obj.x_pos + self.depth == column and obj.y_pos + self.depth == row), False):
-                    if not self.grid[self.board_size * (row - self.depth) + column - self.depth].check_if_hidden():
-                        if self.grid[self.board_size * (row - self.depth) + column - self.depth].check_if_mine():
+                next_cell = next((obj for obj in self.grid if obj.x_pos + self.depth == column and obj.y_pos +
+                                  self.depth == row), False)
+                if next_cell:
+                    element = self.board_x_size * self.board_y_size * (row - self.depth) + column - self.depth
+                    if not next_cell.check_if_hidden():
+                        if next_cell.check_if_mine():
                             item = " #"
                         else:
-                            item = " " + str(self.grid[self.board_size * (row - self.depth) + column - self.depth].check_adjacency())
+                            item = " " + str(next_cell.check_adjacency())
                     else:
                         item = " #"
                 else:
-                    item = " U"
-                    if (column % (int(self.board_size) + 2 * self.depth)) == (int(self.board_size) + 2 * self.depth - 1):
+                    item = " ."
+                    if (column % (int(self.board_x_size) + 2 * self.depth)) == (int(self.board_x_size) + 2 * self.depth - 1):
                         space = " \n"
                     else:
                         space = " "
                 print(item + space, sep=' ', end='', flush=True)
         print("")
 
-    def show_cell(self, x, y):
+    def update_cells_positions(self, board, grid, x, y):
+        if x == 0 or x == board.board_x_size - 1:
+            board.board_x_size += 1
+            for cell in grid:
+                if x == 0:
+                    cell.x_pos += 1
+                cell.board_x_size += 1
+        if y == 0 or y == board.board_y_size - 1:
+            board.board_y_size += 1
+            for cell in grid:
+                if y == 0:
+                    cell.y_pos += 1
+                cell.board_y_size += 1
+        return grid
+
+    def create_cells(self, board, grid, x, y):
+        list_ = []
+        if x == 0 or y == 0 or x == board.board_x_size - 1 or  y == board.board_y_size - 1:
+            self.update_cells_positions(board, grid, x, y)
+        for xx in range(0 if x == 0 else x-1, x+3 if x == 0 else x+2): #x=0: x,x+1
+            for yy in range(0 if y == 0 else y-1, y+3 if y == 0 else y+2): #x=0: y-1, y+2
+                try:
+                    if next((obj for obj in grid if obj.x_pos == xx and obj.y_pos == yy), False):
+                        continue
+                    else:
+                        list_.append(Cell(xx, yy))
+                        print("Cell index: %d")
+                except:
+                    list_.append(Cell(xx, yy))
+                    print("Cell index: %d", )
+        return list_
+
+    def show_cell(self, board, x, y):
         pos = self.grid.index(next(obj for obj in self.grid if (obj.x_pos == x and obj.y_pos == y)))
+        if (not self.check_if_all_neighbors(board, x, y)) or x == 0 or y == 0 or x == board.board_x_size - 1 or  y == board.board_y_size - 1:
+            self.grid += self.grid[pos].create_cells(board, self.grid, x, y)
         if (self.grid[pos].check_adjacency() == 0) and self.grid[pos].check_if_hidden() and not self.grid[pos].check_if_mine():
             self.show_more_cells(pos)
         if self.grid[pos].check_if_mine():
@@ -62,14 +101,14 @@ class Board:
         self.grid[pos].uncover_cell()
         x = self.grid[pos].get_cell_x_pos()
         y = self.grid[pos].get_cell_y_pos()
-        for adj_pos in range(int(self.board_size) ** 2):
+        for adj_pos in range(Cell.counter):
             adj_x = self.grid[adj_pos].get_cell_x_pos()
             adj_y = self.grid[adj_pos].get_cell_y_pos()
             if -1 <= x - adj_x <= 1 and -1 <= y - adj_y <= 1:
                 if int(self.grid[adj_pos].check_if_mine()):
                     no_mine = False
         if no_mine:
-            for adj_pos in range(int(self.board_size) ** 2):
+            for adj_pos in range(Cell.counter):
                 adj_x = self.grid[adj_pos].get_cell_x_pos()
                 adj_y = self.grid[adj_pos].get_cell_y_pos()
                 if -1 <= x - adj_x <= 1 and -1 <= y - adj_y <= 1:
@@ -81,11 +120,20 @@ class Board:
         no_mine = True
         return False
 
+    def check_if_all_neighbors(self, board, x, y):
+        for adj_y_pos in range(y-1, y+2):
+            for adj_x_pos in range(x-1, x+2):
+                if next((True for obj in board.grid if obj.x_pos == adj_x_pos and obj.y_pos == adj_y_pos), False):
+                    continue
+                else:
+                    return False
+        return True
+
     def show_more_cells(self, pos):
         self.grid[pos].uncover_cell()
         x = self.grid[pos].get_cell_x_pos()
         y = self.grid[pos].get_cell_y_pos()
-        for adj_pos in range(int(self.board_size) ** 2):
+        for adj_pos in range(Cell.counter):
             adj_x = self.grid[adj_pos].get_cell_x_pos()
             adj_y = self.grid[adj_pos].get_cell_y_pos()
             if -1 <= x - adj_x <= 1 and -1 <= y - adj_y <= 1:
@@ -96,6 +144,14 @@ class Board:
 
     def return_board_size(self):
         return self.board_size
+
+    def change_board_x_size(self, x):
+        self.board_x_size = x
+        self.board_size = self.board_x_size * self.board_y_size
+
+    def change_board_y_size(self, y):
+        self.board_y_size = y
+        self.board_size = self.board_x_size * self.board_y_size
 
     def return_mines_count(self):
         return self.mines_count
@@ -127,6 +183,7 @@ class Board:
 
 class Cell(Board):
     counter = 0
+
     def __init__(self, x, y):
         Board.__init__(self)
         self.x_pos = x
@@ -189,7 +246,10 @@ def game():
         print("Which cell You want to uncover?")
         x = int(input("Type X: "))
         y = int(input("Type Y: "))
-        board.show_cell(x - 1, y - 1)
+        try:
+            board.show_cell(board, x - 1, y - 1)
+        except:
+            print("Error")
 
 if __name__ == "__main__":
     try:
